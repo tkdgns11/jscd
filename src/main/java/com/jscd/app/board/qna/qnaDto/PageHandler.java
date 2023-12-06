9f450ac53dce07df32e8b8dd0fc743150b11e139
김효정 <kimhyojeong@gimhyojeong-ui-MacBookPro.local>
2023-12-06 15:26:49 +0900

    231206(수) 김효정 QnA기능구현

--- src/main/java/com/jscd/app/board/qna/qnaDto/PageHandler.java
+++ src/main/java/com/jscd/app/board/qna/qnaDto/PageHandler.java
@@ -2,18 +2,25 @@ package com.jscd.app.board.qna.qnaDto;
 
 import org.springframework.web.util.UriComponentsBuilder;
 
+
 public class PageHandler {
     private SearchCondition sc;
 
-    public  final int NAV_SIZE = 10; // page navigation size
-    private int totalCnt; // 게시물의 총 갯수
-    private int totalPage; // 전체 페이지의 갯수
-    private int beginPage; // 화면에 보여줄 첫 페이지
-    private int endPage; // 화면에 보여줄 마지막 페이지
+    public final int NAV_SIZE = 10; // page navigation size
+    private int totalCnt; // 게시물의 총 갯수 page
+    private int totalPage; // 전체 페이지의 갯수 maxPage
+    private int beginPage; // 화면에 보여줄 첫 페이지 BeginPage
+    private int endPage; // 화면에 보여줄 마지막 페이지 endPage
     private boolean showNext = false; // 이후를 보여줄지의 여부. endPage==totalPage이면, showNext는 false
     private boolean showPrev = false; // 이전을 보여줄지의 여부. beginPage==1이 아니면 showPrev는 false
 
+
+
+
+
+
     public PageHandler(int totalCnt, Integer page) {
+
         this(totalCnt, new SearchCondition(page, 10));
     }
 
@@ -28,13 +35,17 @@ public class PageHandler {
         doPaging(totalCnt, sc);
     }
 
+    public PageHandler() {
+
+    }
+
     private void doPaging(int totalCnt, SearchCondition sc) {
-        this.totalPage = totalCnt / sc.getPageSize() + (totalCnt % sc.getPageSize()==0? 0:1);
+        this.totalPage = totalCnt / sc.getPageSize() + (totalCnt % sc.getPageSize() == 0 ? 0 : 1);
         this.sc.setPage(Math.min(sc.getPage(), totalPage));  // page가 totalPage보다 크지 않게
-        this.beginPage = (this.sc.getPage() -1) / NAV_SIZE * NAV_SIZE + 1; // 11 -> 11, 10 -> 1, 15->11. 따로 떼어내서 테스트
+        this.beginPage = (this.sc.getPage() - 1) / NAV_SIZE * NAV_SIZE + 1; // 11 -> 11, 10 -> 1, 15->11. 따로 떼어내서 테스트
         this.endPage = Math.min(beginPage + NAV_SIZE - 1, totalPage);
-        this.showPrev = beginPage!=1;
-        this.showNext = endPage!=totalPage;
+        this.showPrev = beginPage != 1;
+        this.showNext = endPage != totalPage;
     }
 
     public String getQueryString() {
@@ -44,21 +55,21 @@ public class PageHandler {
     public String getQueryString(Integer page) {
         // ?page=10&pageSize=10&option=A&keyword=title
         return UriComponentsBuilder.newInstance()
-                .queryParam("page",     page)
+                .queryParam("page", page)
                 .queryParam("pageSize", sc.getPageSize())
-                .queryParam("option",   sc.getOption())
-                .queryParam("keyword",  sc.getKeyword())
+                .queryParam("option", sc.getOption())
+                .queryParam("keyword", sc.getKeyword())
                 .build().toString();
     }
 
     void print() {
-        System.out.println("page="+ sc.getPage());
-        System.out.print(showPrev? "PREV " : "");
+        System.out.println("page=" + sc.getPage());
+        System.out.print(showPrev ? "PREV " : "");
 
-        for(int i=beginPage;i<=endPage;i++) {
-            System.out.print(i+" ");
+        for (int i = beginPage; i <= endPage; i++) {
+            System.out.print(i + " ");
         }
-        System.out.println(showNext? " NEXT" : "");
+        System.out.println(showNext ? " NEXT" : "");
     }
 
     public SearchCondition getSc() {
