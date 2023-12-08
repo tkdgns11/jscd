98a94f9e39d00d9c9aca7950ef1be5a4e0d231b0
USER <worklikesurf@gmail.com>
2023-12-08 16:39:26 +0900

    231208 김지현

---
+++ src/main/java/com/jscd/app/board/notice/dto/noticeDto.java
@@ -0,0 +1,107 @@
+package com.jscd.app.board.notice.dto;
+
+
+import java.util.Objects;
+
+public class noticeDto {
+    private Integer bno;
+    private String title;
+    private String content;
+    private String writer;
+    private int viewCnt;
+    private String modifyDate;
+    private String regDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        noticeDto noticeDto = (com.jscd.app.board.notice.dto.noticeDto) o;
+        return Objects.equals(bno, noticeDto.bno) && Objects.equals(title, noticeDto.title) && Objects.equals(content, noticeDto.content) && Objects.equals(writer, noticeDto.writer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bno, title, content, writer);
+    }
+
+    @Override
+    public String toString() {
+        return "noticeDto{" +
+                "bno=" + bno +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", writer='" + writer + '\'' +
+                ", viewCnt=" + viewCnt +
+                ", modifyDate='" + modifyDate + '\'' +
+                ", regDate='" + regDate + '\'' +
+                '}';
+    }
+
+    public noticeDto() {
+        this("", "", "");
+    }
+
+    public Integer getBno() {
+        return bno;
+    }
+
+    public void setBno(Integer bno) {
+        this.bno = bno;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public int getViewCnt() {
+        return viewCnt;
+    }
+
+    public void setViewCnt(int viewCnt) {
+        this.viewCnt = viewCnt;
+    }
+
+    public String getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(String modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public noticeDto(String title, String content, String writer) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
+}
+
