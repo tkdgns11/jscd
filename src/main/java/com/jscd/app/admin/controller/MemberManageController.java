a86204007934c6498491459a50fc08484f7fe858
kangjungsu <wjdtnrkd0203@naver.com>
2023-12-26 03:33:34 +0900

    231226 관리자 수정

--- src/main/java/com/jscd/app/admin/controller/MemberManageController.java
+++ src/main/java/com/jscd/app/admin/controller/MemberManageController.java
@@ -52,8 +52,7 @@ public class MemberManageController {
             model.addAttribute("msg", "LIST_ERR");
             return "redirect:/admin/home";
         }
-//        return "/admin/memberManage/memberManageList";
-        return "/admin/memberManage/adminMemberManageList";
+        return "/admin/memberManage/memberManageList";
     }
 
 
